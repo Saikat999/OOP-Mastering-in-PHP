d5c3e4d33ef490d33c62b9a6f6f6b7972a83c81a
Saikat Hasan <saikathasan005@gmail.com>
2024-02-25 15:22:27 +0600

    Added proper comments on codes

--- inheritance.php
+++ inheritance.php
@@ -13,30 +13,33 @@ class Player{
         echo $this->name. " is ".$this->age." years old\n";
     }
 }
- class Cricketer extends Player{
+// Child class Cricketer inherite the parent class Player 
+class Cricketer extends Player{
     private $centuries;
 
     public function __construct($name, $age, $centuries){
 
-        parent::__construct($name,$age);
+        parent::__construct($name,$age); // call the properties of parents class
         $this->centuries = $centuries;
     }
  }
 
+ 
+ // Child class Footballer inherite the parent class Player 
  class Footballer extends Player{
 
     private $goals;
 
     public function __construct($name,$age, $goals){
 
-        parent::__construct($name,$age);
+        parent::__construct($name,$age);  // call the properties of parent class 
         $this->goals = $goals;
     }
  }
 
- //create object
- $cricketer = new Cricketer("Tamim",35,12);
- $footballer = new Footballer("Naymer Jr",32,100);
+ //create objects
+ $cricketer = new Cricketer("Tamim",35,12); //Create cricketer object
+ $footballer = new Footballer("Naymer Jr",32,100); //Create footballer object
 
  echo $cricketer->getPlayerDetails();
  echo $footballer->getPlayerDetails();
